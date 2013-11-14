f39220c0d0d6bae538663eb9ad338d0699c8282a
yuxingfirst <yuxingfirst@gmail.com>
2013-11-14 15:26:13 +0800

    adsfadsf

---
+++ echo/echo_client.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <error.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <signal.h>
+
+void str_cli(FILE *fp, int sfd ) {
+    char sendline[1024], recvline[2014];
+    while( fgets(sendline, 1024, fp) != NULL ) {
+        write(sfd, sendline, strlen(sendline)); 
+        if( read(sfd, recvline, 1024) == 0 ) {
+            printf("server term prematurely.\n"); 
+        }
+		shutdown(fd, SHUT_WR);
+        fputs(recvline, stdout);
+    }
+}
+
+int main() {
+    int s;
+    if( (s = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+        int e = errno; 
+        perror("create socket fail.\n");
+        exit(0);
+    }
+    
+    struct sockaddr_in server_addr, child_addr; 
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(9999);
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+    //inet_pton(AF_INET, "192.168.57.87", &server_addr.sin_addr);
+    
+    
+    if( connect(s, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0 ) {
+        perror("connect fail."); 
+        exit(0);
+    }
+    str_cli(stdin, s);
+    exit(0);
+}
--- echo/echo_server.c
+++ echo/echo_server.c
@@ -9,55 +9,50 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <signal.h>
-#include <sys/types.h>
 
-void sig_handle(int signal) {
-	pid_t pid = getpid();
-	printf("handle signal, pid:%d, signal:%d\n", pid, signal);
+void str_echo(int c ) {
+    ssize_t n;
+    char buf[1024];
+    while( (n = read(c, buf, 1024)) > 0 ) {
+        write(c, buf, n); 
+    }
+    printf("client quit.:%d", n);
 }
 
-int main(void) {
-	signal(SIGPIPE, sig_handle);
+int main() {
     int s, c;
+    pid_t child;
     struct sockaddr_in server_addr, child_addr; 
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(9999);
+    //server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+
     if( (s = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
         perror("create socket fail.");
         exit(0);
     }
+
     if( bind(s, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0 ) {
         perror("bind fail.");
         exit(0); 
     }
+    
     if( listen(s, 1024) < 0 ) {
         perror("listen fail.");
         exit(0); 
     }
+
     printf("start server...\n");
     for (;;) {
         socklen_t clilen = sizeof(child_addr);
         c = accept(s, (struct sockaddr *)&child_addr, &clilen);
-        if( fork() == 0 ) {
-			pid_t child = getpid();
-			printf("In child process :%d\n", child);
-			//首先调用close, 然后再注释掉close,调用shutdown
-			close(c); 
-            //shutdown(c, SHUT_WR);
-            exit(0);
-        }
-		//让子进程先执行
-        sleep(5);
-		pid_t parent = getpid();
-		printf("In parent process:%d\n", parent);
-		const char *buf = "server parent process send data";
-		int n = write(c, buf, strlen(buf));
-		if(n < 0) {
-			printf("%d", errno);
-		}
-		close(c);
+
+        str_echo(c);    
+        sleep(10);
+        printf("close client.\n");
+        close(c);
     }
-    return 0;
+    exit(0);
 }
